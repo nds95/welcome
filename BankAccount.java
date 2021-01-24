e03f30ce9047c2f49f5c424167c986a4cd0c9834
nds95 <69199786+nds95@users.noreply.github.com>
2021-01-24 23:05:42 +0900

    Add access modifier

--- BankAccount.java
+++ BankAccount.java
@@ -1,18 +1,38 @@
 public class BankAccount {
-    int balance;
-    Person owner;
+    private int balance;
+    private Person owner;
+
+    public void setBalance(int newBalance) {
+        if (newBalance >= 0) {
+            balance = newBalance;
+        }
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setOwner(Person newOwner) {
+        owner = newOwner;
+    }
+
+    public Person getOwner() {
+        return owner;
+    }
 
     // 파라미터 : 입금할 액수(int)
     // 리턴값 : 성공 여부(boolean)
 
     boolean deposit(int amount) {
-        if (amount < 0 || owner.cashAmount < amount) {
-            System.out.println("입금 실패입니다. 잔고: " + balance + "원, 현금: " + owner.cashAmount);
+        if (amount < 0 || owner.getCashAmount() < amount) {
+            System.out.println("입금 실패입니다. 잔고: " + balance + "원, 현금: " + owner.getCashAmount() + "원");
+            System.out.println("false");
             return false;
         } else {
             balance += amount;
-            owner.cashAmount -= amount;
-            System.out.println(amount + "원 입금하였습니다. 잔고: " + balance + "원, 현금: " + owner.cashAmount);
+            owner.setCashAmount(owner.getCashAmount() - amount);
+            System.out.println(amount + "원 입금하였습니다. 잔고: " + balance + "원, 현금: " + owner.getCashAmount() + "원");
+            System.out.println("true");
             return true;
         }
     }
@@ -21,12 +41,14 @@ public class BankAccount {
     // 리턴값 : 성공 여부(boolean)
     boolean withdraw(int amount) {
         if (amount < 0 || amount > balance) {
-            System.out.println("출금 실패입니다. 잔고: " + balance + "원, 현금: " + owner.cashAmount);
+            System.out.println("출금 실패입니다. 잔고: " + balance + "원, 현금: " + owner.getCashAmount() + "원");
+            System.out.println("false");
             return false;
         } else {
             balance -= amount;
-            owner.cashAmount += amount;
-            System.out.println(amount + "원 출금하였습니다. 잔고: " + balance + "원, 현금: " + owner.cashAmount);
+            getOwner().setCashAmount(getOwner().getCashAmount() + amount);
+            System.out.println(amount + "원 출금하였습니다. 잔고: " + balance + "원, 현금: " + owner.getCashAmount() + "원");
+            System.out.println("true");
             return true;
         }
     }
--- BankDriver.java
+++ BankDriver.java
@@ -4,26 +4,26 @@ public class BankDriver {
     public static void main(String[] args) {
         // 사람 선언
         Person p1 = new Person();
-        p1.name = "김신의";
-        p1.age = 28;
-        p1.cashAmount = 30000;
+        p1.setName("김신의");
+        p1.setAge(28);
+        p1.setCashAmount(30000);
 
-        // 은행 계좌 생성
+        // 은행 계좌 생성EE
         BankAccount a1 = new BankAccount();
-        a1.balance = 100000;
+        a1.setBalance(1000000);
 
-        p1.account = a1;
-        a1.owner = p1;
+        p1.setAccount(a1);
+        a1.setOwner(p1);
 
         Person p2 = new Person();
-        p2.cashAmount = 100000;
+        p2.setCashAmount(100000);
 
         BankAccount a2 = new BankAccount();
-        a2.balance = 500000;
+        a2.setBalance(500000);
 
 
-        p2.account = a2;
-        a2.owner = p2;
+        p2.setAccount(a2);
+        a2.setOwner(p2);
 
         a2.deposit(30000);
         a2.withdraw(170000);
